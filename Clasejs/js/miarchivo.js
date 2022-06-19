56dc69e75dc8b3d92760edf69f9e8691e6771ede
DanielaOviedo <ovdaniela89.8@gmail.com>
2022-06-18 23:59:33 -0300

    DOM

--- Clasejs/js/miarchivo.js
+++ Clasejs/js/miarchivo.js
@@ -9,5 +9,18 @@ function  calculo(){
     alert(nombre + " si corriste consumiste: " + corre + " calorías");
     alert("Gracias por confiar!")
 }
+const panels = document.querySelectorAll('.panel');
+
+        function toggleOpen(){
+            this.classList.toggle('open');
+        }
+
+        function toggleActive(e){
+            if(e.propertyName.includes('flex-grow')){
+                this.classList.toggle('open-active'); 
+            }
+        }
+        panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+        panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
 

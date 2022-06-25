f5b3b77c8eda29ae664dcf9916773486febd4d05
DanielaOviedo <ovdaniela89.8@gmail.com>
2022-06-25 20:38:46 -0300

    localstorage

--- Clasejs/js/miarchivo.js
+++ Clasejs/js/miarchivo.js
@@ -1,26 +1,16 @@
-function  calculo(){
-    let nombre = prompt("Ingresa tu nombre");
-    let modo = prompt("Corriste o Caminaste?");
-    let kilometros = prompt("Cuántos kilómetros?");
-    let camina = (kilometros*46.22);
-    let corre = (kilometros*67.9);
-
-    alert(nombre + " si caminaste consumiste: " + camina + " calorías");
-    alert(nombre + " si corriste consumiste: " + corre + " calorías");
-    alert("Gracias por confiar!")
+function contadorClicks() {
+    // Si existe contador se incrementa y sino se inicializa
+    if (localStorage.contador) {
+        // Se convierte el contador a entero porque por defecto es una cadena
+        localStorage.contador = parseInt(localStorage.contador) + 1;
+    } else {
+        localStorage.contador = 1;
+    }
+    document.getElementById("contador").value = localStorage.contador + " En carrito";
 }
-const panels = document.querySelectorAll('.panel');
 
-        function toggleOpen(){
-            this.classList.toggle('open');
-        }
-
-        function toggleActive(e){
-            if(e.propertyName.includes('flex-grow')){
-                this.classList.toggle('open-active'); 
-            }
-        }
-        panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-        panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
-
-
+function resetearClicks() {
+    localStorage.removeItem("contador");
+    document.getElementById("contador").value = "Vacio";
+    
+}
